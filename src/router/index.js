d5c6b23e127e2e8b92cf7d3f4d4f340c13948d57
HeiShen <3142105474@qq.com>
2025-06-05 19:42:31 +0800

    commit

--- src/router/index.js
+++ src/router/index.js
@@ -13,10 +13,6 @@ const router = createRouter({
     },
     {
       path: '/orders',
-<<<<<<< HEAD
-=======
-
->>>>>>> e183441b4254db8680e5b1f26cf6ac0f7055a9ae
       component: orders,
     },
     {
