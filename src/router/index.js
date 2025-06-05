6e7338e2426ea08a139f47255024535b48e6254d
HeiShen <3142105474@qq.com>
2025-06-05 18:54:42 +0800

    commit

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import user from '../views/user.vue'
 import orders from '../views/orders.vue'
 import products from '../views/products.vue'
-
+import category1 from '../views/category1.vue'
+import category2 from '../views/category2.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -12,9 +13,6 @@ const router = createRouter({
     },
     {
       path: '/orders',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: orders,
     },
     {
