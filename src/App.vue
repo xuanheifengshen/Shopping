891ec64ec3c6751be17f084ac8481cf9ce04cbc4
HeiShen <3142105474@qq.com>
2025-06-05 20:15:39 +0800

    branch1test

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-
+      <div>branch1</div>
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
